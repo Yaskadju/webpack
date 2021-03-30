3d0e6592b5b208e4e10fa2f77d3a6ad22ed9e3cf
Thales <thales.y@gmail.com>
2021-03-30 11:20:41 -0300

    final stage

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,19 @@
+const currentTask = process.env.npm_lifecycle_event
 const path = require("path")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const HtmlWebpackPLugin = require("html-webpack-plugin")
+const { CleanWebpackPlugin } = require("clean-webpack-plugin")
+const { WebpackManifestPlugin } = require("webpack-manifest-plugin")
 
-module.exports = {
+const config = {
   entry: "./app/app.js",
   output: {
-    filename: "myBundle.js",
+    filename: "myBundle.[hash].js",
     path: path.resolve(__dirname, "dist")
   },
+  plugins: [new HtmlWebpackPLugin({ template: "./app/index.html" })],
+  mode: "development",
+  devtool: "eval-cheap-source-map",
   devServer: {
     port: 8080,
     contentBase: path.resolve(__dirname, "dist"),
@@ -23,10 +31,18 @@ module.exports = {
         use: {
           loader: "babel-loader",
           options: {
-            presets: ["@babel/preset-env", "@babel/preset-react"]
+            presets: [["@babel/preset-env", { useBuiltIns: "usage", corejs: "3", targets: "defaults" }], "@babel/preset-react"]
           }
         }
       }
     ]
   }
 }
+
+if (currentTask == "build") {
+  config.mode = "production"
+  config.module.rules[0].use[0] = MiniCssExtractPlugin.loader
+  config.plugins.push(new MiniCssExtractPlugin({ filename: "main.[hash].css" }), new CleanWebpackPlugin(), new WebpackManifestPlugin())
+}
+
+module.exports = config
